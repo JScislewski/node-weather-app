0e76bd3ad8c102f0f6f56fd50b14027588cb0aba
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-28 00:22:26 +0200

    Add using geocode api

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 require("dotenv").config();
 const request = require("request");
-const url = `http://api.weatherstack.com/current?access_key=${process.env.API}&query=Gdansk`;
-request({ url, json: true }, (error, response) => {
-  console.log(response.body.current);
+
+const weatherURL = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=Gdansk`;
+
+request({ url: weatherURL, json: true }, (error, response) => {
+  console.log(
+    `${response.body.current.weather_descriptions[0]}\nIt is currenty ${response.body.current.temperature} degrees. It feels like ${response.body.current.feelslike} degrees`
+  );
+});
+
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.GEOCODE_API}&limit=1`;
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+  console.log(latitude, longitude);
 });
