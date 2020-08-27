ef6d7fff6da527e44df73315a2c8d23ea9010953
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-27 23:35:13 +0200

    Add json:true to request

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const request = require("request");
 const url = `http://api.weatherstack.com/current?access_key=${process.env.API}&query=Gdansk`;
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url, json: true }, (error, response) => {
+  console.log(response.body.current);
 });
