5dd71db075731d1986a45317fcad9274c5fa6e8a
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-28 02:51:50 +0200

    Add callback chaining

--- app.js
+++ app.js
@@ -1,35 +1,16 @@
 require("dotenv").config();
 const geocode = require("./utils/geocode");
-
-const weatherURL = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=40.7831,-73.9712`;
-
-/* request({ url: weatherURL, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to weather service!");
-  } else if (response.body.error) {
-    console.log(response.body.error.info);
-  } else {
-    console.log(
-      `${response.body.current.weather_descriptions[0]}\nIt is currenty ${response.body.current.temperature} degrees. It feels like ${response.body.current.feelslike} degrees`
-    );
-  }
-});
-
-request({ url: geocodeURL, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to location service!");
-  } else if (response.body.message) {
-    console.log("Location not found");
-  } else if (!response.body.features.length) {
-    console.log("Location not found");
-  } else {
-    const latitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(latitude, longitude);
-  }
-}); */
+const forecast = require("./utils/forecast");
 
 geocode("Gdansk", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
+  if (error) {
+    return console.log(error);
+  }
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error) {
+      return error;
+    }
+    console.log(data.location);
+    console.log(forecastData);
+  });
 });
--- utils/geocode.js
+++ utils/geocode.js
@@ -11,7 +11,7 @@ const geocode = (address, callback) => {
     } else {
       callback(undefined, {
         latitude: response.body.features[0].center[1],
-        longitute: response.body.features[0].center[0],
+        longitude: response.body.features[0].center[0],
         location: response.body.features[0].place_name,
       });
     }
