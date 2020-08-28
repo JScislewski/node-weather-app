edb924d22ebc3d001fedf4d51b7686ab95344efa
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-28 03:01:20 +0200

    Add yargs

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 require("dotenv").config();
+const yargs = require("yargs");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Gdansk", (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
-    if (error) {
-      return error;
-    }
-    console.log(data.location);
-    console.log(forecastData);
-  });
+yargs.version("1.0");
+
+yargs.command({
+  command: "forecast",
+  describe: "Get forecast",
+  builder: {
+    location: {
+      describe: "Location name",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    geocode(argv.location, (error, data) => {
+      if (error) {
+        return console.log(error);
+      }
+      forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+          return error;
+        }
+        console.log(data.location);
+        console.log(forecastData);
+      });
+    });
+  },
 });
+
+yargs.parse();
